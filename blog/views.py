0ca2126d2120bf5f301a01b9ea38e37f26280861
AnnaMrowca <amrowca@gmail.com>
2021-03-26 15:59:10 +0100

    developing first form for create section

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from blog.models import Post
+from blog.forms import PostForm
 
 def home(request):
     posts = Post.objects.all()
     return render(request, 'home.html', {'posts': posts})
 
 def create(request):
-    return render(request, 'create.html')
+    return render(request, 'create.html', {'form':PostForm()})
